b312f73582a4515889e6d3a6d63598bf8b497e01
Lidiya Morshchinina <lmorshchinka@gmail.com>
2022-08-01 23:47:05 +0300

    code refactoring: delete unnessesary code

--- vue.config.js
+++ vue.config.js
@@ -1,73 +1,13 @@
-// const { defineConfig } = require('@vue/cli-service');
-// module.exports = defineConfig({
-//     transpileDependencies: true,
-//     css: {
-//         sourceMap: true,
-//         loaderOptions: {
-//             sass: {
-//                 additionalData: '@import "@/assets/style/main.sass"',
-//             },
-//         },
-//     },
-//     // module: {
-//     //     rules: [
-//     //         {
-//     //             test: /\.s[ac]ss$/i,
-//     //             use: [
-//     //                 'style-loader',
-//     //                 'css-loader',
-//     //                 {
-//     //                     loader: 'sass-loader',
-//     //                     options: {
-//     //                         // Prefer `dart-sass`
-//     //                         implementation: require('sass'),
-//     //                     },
-//     //                 },
-//     //             ],
-//     //         },
-//     //     ],
-//     // },
-// });
-module.exports = {
+const { defineConfig } = require('@vue/cli-service');
+module.exports = defineConfig({
+    transpileDependencies: true,
     css: {
         sourceMap: true,
         loaderOptions: {
             sass: {
                 // eslint-disable-next-line quotes
-                additionalData: `@import "~@/assets/styles/main.scss"`,
+                additionalData: `@import "~@/assets/styles/main.scss";`,
             },
         },
     },
-
-    // configureWebpack: {
-    //     module: {
-    //         rules: [
-    //             {
-    //                 test: /\.s[ac]ss$/i,
-    //                 use: [
-    //                     // Creates `style` nodes from JS strings
-    //                     'style-loader',
-    //                     // Translates CSS into CommonJS
-    //                     'css-loader',
-    //                     // Compiles Sass to CSS
-    //                     {
-    //                         loader: 'sass-loader',
-    //                         options: {
-    //                             indentedSyntax: true,
-    //                             implementation: require('sass'),
-    //                             // sass-loader >= 8
-    //                             sassOptions: {
-    //                                 indentedSyntax: true,
-    //                             },
-    //                         },
-    //                         // options: {
-    //                         //     // Prefer `dart-sass`
-    //                         //     implementation: require('sass'),
-    //                         // },
-    //                     },
-    //                 ],
-    //             },
-    //         ],
-    //     },
-    // },
-};
+});
